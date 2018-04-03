853182ad33a384ab779f04b2fa696eee17532d53
Vlademir Eleuterio <vla.eleut@gmail.com>
2018-04-03 00:00:31 -0300

    Mais exercícios concluídos da lista 1 (1, 2, 4, 5, 6)

---
+++ L1E04 Bissexto/ConsoleApp5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+/*
+Escreva um programa que receba um número, 
+calcule se ele é bissexto ou não e imprima na tela. 
+Um ano é bissexto se 
+é múltiplo de quatro, não é múltiplo de 100 (1900 não é bissexto) 
+ou é múltiplo de 400 (2000 é bissexto).
+*/
+
+
+namespace L1E04Bissexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um ano para descobrir se ele é bissexto: ");
+            int ano = int.Parse(Console.ReadLine());
+
+            if(ano%400 == 0)
+            {
+                Console.Clear(); 
+                Console.Write("O ano {0} é bissexto.", ano);
+            }else if (ano%4 == 0 && ano%100 != 0)
+            {
+                Console.Clear();
+                Console.Write("O ano {0} é bissexto.", ano);
+            }
+            else
+            {
+                Console.Clear();
+                Console.Write("O ano {0} NÃO é bissexto.", ano);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ L1E06 Porcentagem Caracteres/L01E06 Porcentagem Caracteres/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01E06_Porcentagem_Caracteres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira uma frase qualquer: ");
+            string frase = Console.ReadLine();
+
+            int contVogal = 0, contConsoante = 0;
+            double soma;
+
+            int[] vogais = { 65, 69, 73, 79, 85, 97, 101, 105, 111, 117 };
+                            //vogais maiúsculas e minúsculas na tabela ASCII
+
+            foreach (char c in frase)
+            {
+                if((c >= 65 && c <= 90) | (c >= 97 && c <= 122))
+                    //vou considerar apenas as letras. espaços, caracteres especiais e números não contam.
+                {
+                    for (int i=0; i< vogais.Length; i++)
+                    {
+                        if ((int)c == vogais[i])
+                        {
+                            //Console.WriteLine(c + "somei uma V");
+                            contVogal++;
+                            break;
+                        }else if(i == (vogais.Length - 1))
+                        {
+                            //Console.WriteLine(c + "somei uma C");
+                            contConsoante++;
+                        }
+                    }
+                }
+            }
+            soma = contConsoante + contVogal;
+            Console.Clear();
+            Console.WriteLine("Na frase \"" + frase + "\" existem:\n {0} Vogais ({1}%) e {2} consoantes ({3}%)", 
+                contVogal, Math.Round((contVogal/soma*100) ,2), contConsoante, Math.Round(contConsoante / soma * 100, 2));
+            Console.Read();
+        }
+    }
+}
+//letras maiúsculas: 65-90
+          // vogais: 65,69,73,79,85
+//letras minúsculas: 97-122
+       //consoantes: 97,101, 105, 111, 117
